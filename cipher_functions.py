58032f735736b7639a1ffcc4f9e015a07e52547e
akhomlyuk <relicbs@gmail.com>
2023-04-03 22:52:11 +0300

    v0.2 added russian alphabet

--- cipher_functions.py
+++ cipher_functions.py
@@ -1,14 +1,21 @@
-upper_alp = {key: chr(value) for key, value in zip(range(26), range(65, 91))}
-lower_alp = {key: chr(value) for key, value in zip(range(26), range(97, 123))}
+upper_english_alp = {key: chr(value) for key, value in zip(range(26), range(65, 91))}
+lower_english_alp = {key: chr(value) for key, value in zip(range(26), range(97, 123))}
+
+upper_russian_alp = {key: chr(value) for key, value in zip(range(32), range(1040, 1072))}
+lower_russian_alp = {key: chr(value) for key, value in zip(range(32), range(1072, 1104))}
 
 
 def encrypt(original_string: str, shift_key: int) -> str:
     enc = ''
     for i in original_string:
-        if i.isupper() and i.isalpha():
-            enc += upper_alp.get(((ord(i) + shift_key - ord('A')) % 26))
-        elif i.lower() and i.isalpha():
-            enc += lower_alp.get(((ord(i) + shift_key - ord('a')) % 26))
+        if i.isupper() and i.isalpha() and i in upper_english_alp.values():
+            enc += upper_english_alp.get(((ord(i) + shift_key - ord('A')) % 26))
+        elif i.lower() and i.isalpha() and i in lower_english_alp.values():
+            enc += lower_english_alp.get(((ord(i) + shift_key - ord('a')) % 26))
+        elif i.isupper() and i.isalpha() and i in upper_russian_alp.values():
+            enc += upper_russian_alp.get(((ord(i) + shift_key - ord('А')) % 32))
+        elif i.lower() and i.isalpha() and i in lower_russian_alp.values():
+            enc += lower_russian_alp.get(((ord(i) + shift_key - ord('а')) % 32))
         else:
             enc += i
     return enc
@@ -17,10 +24,14 @@ def encrypt(original_string: str, shift_key: int) -> str:
 def decrypt(encrypted_string: str, shift_key: int) -> str:
     dec = ''
     for i in encrypted_string:
-        if i.isupper() and i.isalpha():
-            dec += upper_alp.get(((ord(i) - shift_key - ord('A')) % 26))
-        elif i.lower() and i.isalpha():
-            dec += lower_alp.get(((ord(i) - shift_key - ord('a')) % 26))
+        if i.isupper() and i.isalpha() and i in upper_english_alp.values():
+            dec += upper_english_alp.get(((ord(i) - shift_key - ord('A')) % 26))
+        elif i.lower() and i.isalpha() and i in lower_english_alp.values():
+            dec += lower_english_alp.get(((ord(i) - shift_key - ord('a')) % 26))
+        elif i.isupper() and i.isalpha() and i in upper_russian_alp.values():
+            dec += upper_russian_alp.get(((ord(i) - shift_key - ord('А')) % 32))
+        elif i.lower() and i.isalpha() and i in lower_russian_alp.values():
+            dec += lower_russian_alp.get(((ord(i) - shift_key - ord('а')) % 32))
         else:
             dec += i
     return dec
--- main.py
+++ main.py
@@ -10,21 +10,21 @@ if user_choice == 'D' or user_choice == 'd':
     ciphered_string = input(Fore.CYAN + 'Text for decryption: ')
     n = int(input(Fore.CYAN + 'Shift(any positive or negative integer): '))
     print(f'{Fore.BLUE}Decrypted text: {Fore.GREEN}{decrypt(ciphered_string, n)}')
-    with open('decrypted.txt', 'w') as file:
+    with open('decrypted.txt', 'w', encoding='UTF-8') as file:
         file.writelines(decrypt(ciphered_string, n))
     print(f'Text saved to: decrypted.txt')
 elif user_choice == 'E' or user_choice == 'e':
     string_for_encrypt = input(Fore.CYAN + 'Text for encryption: ')
     n = int(input(Fore.CYAN + 'Shift(any positive or negative integer): '))
     print(f'{Fore.RED}Encrypted text: {Fore.GREEN}{encrypt(string_for_encrypt, n)}')
-    with open('encrypted.txt', 'w') as file:
+    with open('encrypted.txt', 'w', encoding='UTF-8') as file:
         file.writelines(encrypt(string_for_encrypt, n))
     print(f'Text saved to: encrypted.txt')
 elif user_choice == 'B' or user_choice == 'b':
     ciphered_string = input(Fore.CYAN + 'Text for decryption: ')
     for i in range(1, 27):
         ls = ['ROT key ' + str(i) + ': ' + decrypt(ciphered_string, i) + '\n' for i in range(1, 26)]
-        with open('bruted.txt', 'w') as file:
+        with open('bruted.txt', 'w', encoding='UTF-8') as file:
             file.writelines(ls)
         print(f'{Fore.CYAN}ROT key {Fore.GREEN}{i}: {Style.RESET_ALL}{Style.BRIGHT}{decrypt(ciphered_string, i)}')
     print(f'Text saved to: bruted.txt')
